03e7e44ca4a76ee7e1c7ea51c016002b5ea498e8
Ancient <kacper.blazejewski88@gmail.com>
2024-03-23 20:37:08 +0100

    navbarchanges

--- navbar.php
+++ navbar.php
@@ -15,10 +15,13 @@ function displayNavbar() {
             <div class="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link" href="register.php">Rejestracja</a>
+                    '.getRegisterLink() .'
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Logowanie</a>
+                        '.getLoginLogoutLink() .'
+                    </li>
+                    <li class="nav-item">
+                    '.getProfileLink() .'
                     </li>
                     <li class="nav-item">
                         <button class="btn mb-2" id="btn-reservation">Rezerwuj tor</button>
@@ -27,4 +30,27 @@ function displayNavbar() {
             </div>
         </div>  
     </nav>';
+}
+function getLoginLogoutLink(){
+    if (isUserLoggedIn()) {   
+        return '<a class="nav-link" href="logout.php">Wyloguj</a>';
+    } else {
+        return '<a class="nav-link" href="login.php">Logowanie</a>';
+    }
+}
+
+function getProfileLink() {
+    if (isUserLoggedIn()) {
+        return '<a class="nav-link" href="profile.php">Profil</a>';
+    } else {
+        return ''; // Ukrywamy link do profilu, jeśli użytkownik nie jest zalogowany
+    }
+}
+
+function getRegisterLink() {
+    if (isUserLoggedIn()) {
+        return ''; // Ukrywamy link do rejestracji, jeśli użytkownik jest zalogowany
+    } else {
+        return '<a class="nav-link" href="register.php">Rejestracja</a>';
+    }
 }
