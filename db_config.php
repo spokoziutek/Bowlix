48e70c050a35582bae91f647325b7a5d172ef6ac
Ancient <kacper.blazejewski88@gmail.com>
2024-03-23 21:38:42 +0100

    sss

--- db_config.php
+++ db_config.php
@@ -1,17 +1,2 @@
-<!-- <?php
-// Dane do połączenia z bazą
-$db_host = 'eu-cluster-west-01.k8s.cleardb.net';  // Host bazy danych (często localhost)
-$db_name = 'heroku_3d9b81ee31eb9ad';     // Nazwa bazy danych
-$db_user = 'b1bf96cb456b47'; // Nazwa użytkownika
-$db_password = '5a731034';  // Hasło
 
-// Nawiązanie połączenia
-try {
-    $db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_password);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Włączanie wyjątków w przypadku błędów PDO
-} catch (PDOException $e) {
-    echo "Błąd połączenia z bazą danych: " . $e->getMessage();
-    exit(); // Wyświetlanie błędu i przerywanie dalszego wykonywania
-}
-?> -->
 
